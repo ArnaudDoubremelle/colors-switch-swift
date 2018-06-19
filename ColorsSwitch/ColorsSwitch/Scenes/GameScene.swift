3b566ebb346922cf945ee583a0dd31e59a54bade
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2018-06-19 12:02:29 +0200

    Project setup

---
+++ ColorsSwitch/ColorsSwitch/Scenes/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  ColorsSwitch
+//
+//  Created by Baptiste BRIOIS on 19/06/2018.
+//  Copyright © 2018 ADBB. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
---
+++ ColorsSwitch/ColorsSwitch/ViewController/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  ColorsSwitch
+//
+//  Created by Baptiste BRIOIS on 19/06/2018.
+//  Copyright © 2018 ADBB. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+                
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
