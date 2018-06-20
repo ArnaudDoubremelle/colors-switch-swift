0b3570c8e987490619998dc19e2d5a22b130cfad
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2018-06-20 17:37:55 +0200

    Menu and game

---
+++ ColorsSwitch/ColorsSwitch/Template/Settings.swift
@@ -0,0 +1,21 @@
+//
+//  Settings.swift
+//  ColorsSwitch
+//
+//  Created by Baptiste BRIOIS on 20/06/2018.
+//  Copyright © 2018 ADBB. All rights reserved.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1           // 01
+    static let switchCategory: UInt32 = 0x1 << 1    // 10
+}
+
+enum ZPositions {
+    static let label: CGFloat = 0
+    static let ball: CGFloat = 1
+    static let colorSwitch: CGFloat = 2
+}
